e41ed06aae3da9d46b2b9b71beb368de674f830c
Danuxy-Studio <daunuxydigitalgroup@gmail.com>
2026-06-09 19:04:35 +0700

    FIx Performa

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ app.use((req, res, next) => {
     res.setHeader('Cache-Control', 'public, max-age=3600');
   }
   res.setHeader('X-Content-Type-Options', 'nosniff');
+  res.setHeader('X-Frame-Options', 'SAMEORIGIN');
   next();
 });
 
